fb2c3f648d7112143f4f61a767602a4b9943d5f5
jayliu <879303561@qq.com>
2022-07-19 22:37:48 +0800

    lr2

--- LR.py
+++ LR.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
 import numpy as np
+from sklearn.metrics import roc_auc_score
 import os
 
 df_train = pd.read_csv("dx_train.csv")
@@ -11,9 +12,10 @@ scaler = StandardScaler()
 df_train[std_cols] = scaler.fit_transform(df_train[std_cols])
 
 class LR:
-    def __init__(self, epoch=5, learning_rate=0.1):
+    def __init__(self, epoch=5, learning_rate=0.001, lambda_=0.1):
         self.learning_rate = learning_rate
         self.epoch = epoch
+        self.lambda_ = lambda_
 
     def fit(self, X, y):
         ones = np.ones((X.shape[0], 1))
@@ -24,13 +26,18 @@ class LR:
             loss = self._loss(out, self.y)
             self.grd = self._cal_gradients(out)
             self._update_weight()
-            print(epoch, loss)
+            if epoch % 100 == 0:
+                acores = self.predprob(self.X)
+                auc = roc_auc_score(self.y, acores)
+                print(epoch, loss, auc)
+
 
     def predict(self, X):
         pass
 
     def predprob(self, x):
-        pass
+        logits = np.sum(-self.weights * x, axis=1)
+        return self._sigmoid(logits)
 
     def _init_weights(self, X):
         col_num = X.shape[-1] + 1
@@ -54,14 +61,18 @@ class LR:
         negative_out[mask] = 0
 
         return positive_out + negative_out
-        # return 1 / (np.exp(x) + 1)
 
     def _predict(self):
         logits = np.sum(-self.weights * self.X, axis=1)
         return self._sigmoid(logits)
 
     def _update_weight(self):
+        # print("************************")
+        # print(self.weights)
+        # print("************* grd **************")
+        # print(self.grd)
         self.weights = self.weights - self.learning_rate * self.grd
+        # print(self.weights)
 
     def _loss(self, out, y):
         loss_vals = y * np.log(out + 1e-4) + (1 - y) * np.log((1 - out + 1e-4))
@@ -72,10 +83,25 @@ class LR:
 
     def _cal_gradients(self, out):
         grd = (self.y - out) * np.transpose(self.X)
-        return grd.sum(axis=1)
+        grd_norm = self.lambda_ * self.weights
+        grd = grd.mean(axis=1) + grd_norm
+        return grd
 
 if __name__ == '__main__':
+    from sklearn.linear_model import LogisticRegression
+
+
     y_train = df_train["是否流失"]
     X_train = df_train.drop(columns=["是否流失"])
-    lr = LR(epoch=100)
+
+    # lr = LogisticRegression(verbose=1)
+    # lr.fit(X_train, y_train)
+    # scores = lr.predict_proba(X_train)
+    # auc = roc_auc_score(y_train, scores[:, 1])
+    # print(auc)
+
+
+    lr = LR(epoch=10000, learning_rate=0.1)
     lr.fit(X_train.values, y_train.values)
+
+
